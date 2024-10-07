ede5f6204c2791da95f7dab7a6c39f5172c05f7c
Kieran Lowe <11875232+kieran-lowe@users.noreply.github.com>
2024-10-07 21:52:06 +0000

    refactor\!: remove community files
    Not really relevant for this type of project

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::123456789012:role/role-name"
+    session_name = "session-name"
     web_identity_token_file = "/path/to/token"
   }
 
