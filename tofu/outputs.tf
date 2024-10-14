546b8eed3564f35491689fa7a845054b094acdab
Kieran Lowe <11875232+kieran-lowe@users.noreply.github.com>
2024-10-14 20:49:18 +0000

    fix: corect sg resources

--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -1,4 +1,4 @@
 output "grafana_ip" {
-  value = "http://${aws_instance.grafana_server.public_ip}:3000"
+  value       = "http://${aws_instance.grafana_server.public_ip}:3000"
   description = "URL to access the Grafana server"
 }
--- tofu/security_groups.tf
+++ tofu/security_groups.tf
@@ -8,23 +8,23 @@ resource "aws_security_group" "gitops_sg" {
   }
 }
 
-resource "aws_security_group_ingress_rule" "gitops_sg_ingress_rule" {
+resource "aws_vpc_security_group_ingress_rule" "gitops_sg_ingress_rule" {
   security_group_id = aws_security_group.gitops_sg.id
   from_port         = 3000
   to_port           = 3000
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  ip_protocol       = "tcp"
+  cidr_ipv4         = ["0.0.0.0/0"]
 
   tags = {
     Name = "gitops-sg-ingress"
   }
 }
 
-resource "aws_security_group_ingress_rule" "gitops_sg_egress_rule" {
+resource "aws_vpc_security_group_ingress_rule" "gitops_sg_egress_rule" {
   security_group_id = aws_security_group.gitops_sg.id
 
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  ip_protocol = "-1"
+  cidr_ipv4   = ["0.0.0.0/0"]
 
   tags = {
     Name = "gitops-sg-egress"
