53b3189dc601a09e032e3e7c39a1d49fe56fe351
Kieran Lowe <11875232+kieran-lowe@users.noreply.github.com>
2024-10-15 20:48:42 +0000

    feat: improve workflows

--- tofu/provider.tf
+++ tofu/provider.tf
@@ -7,6 +7,20 @@ terraform {
       version = ">= 5.0.0, < 6.0.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "mtc-gitops2024-terraform-state-a12b"
+    encrypt        = true
+    kms_key_id     = "arn:aws:kms:eu-west-2:195275655961:key/77235df8-1d9e-4340-af90-e95a615ce943"
+    region         = "eu-west-2"
+    dynamodb_table = "mtc-gitops2024-terraform-state-locks-a12b"
+    assume_role_with_web_identity = {
+      role_arn                = "arn:aws:iam::195275655961:role/mtc-gitops2024-terraform-state-role"
+      session_name            = "mtc-gitops2024-state-access"
+      web_identity_token_file = "/tmp/web-identity-token"
+      duration                = "15m"
+    }
+  }
 }
 
 provider "aws" {
@@ -15,7 +29,8 @@ provider "aws" {
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::123456789012:role/role-name"
     session_name            = "session-name"
-    web_identity_token_file = "/path/to/token"
+    web_identity_token_file = "/tmp/web-identity-token"
+    duration = "15m"
   }
 
   default_tags {
