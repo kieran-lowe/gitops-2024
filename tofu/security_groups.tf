be9f8c202bdbf53391397a36428ba5b271903673
Kieran Lowe <11875232+kieran-lowe@users.noreply.github.com>
2024-10-16 19:20:51 +0000

    fix: checkov findings

--- tofu/security_groups.tf
+++ tofu/security_groups.tf
@@ -10,6 +10,7 @@ resource "aws_security_group" "gitops_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "gitops_sg_ingress_rule" {
   security_group_id = aws_security_group.gitops_sg.id
+  description = "Allow inbound traffic on port 3000"
   from_port         = 3000
   to_port           = 3000
   ip_protocol       = "tcp"
@@ -23,10 +24,19 @@ resource "aws_vpc_security_group_ingress_rule" "gitops_sg_ingress_rule" {
 resource "aws_vpc_security_group_ingress_rule" "gitops_sg_egress_rule" {
   security_group_id = aws_security_group.gitops_sg.id
 
+  description = "AWS-default egress rule"
   ip_protocol = "-1"
   cidr_ipv4   = "0.0.0.0/0"
 
   tags = {
     Name = "gitops-sg-egress"
+  }
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.gitops_vpc.id
+
+  tags = {
+    Name = "default-sg"
   }
 }
